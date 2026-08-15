975c8c10af67169d17c8781a471fcf1de9f04570
wqf31415 <wqf31415@users.noreply.github.com>
2026-08-15 10:16:58 +0000

    deploy: f84a26a0ed43b5cebc8c83b9e4a2146e76e89ed8

--- js/third-party/math/mathjax.js
+++ js/third-party/math/mathjax.js
@@ -4,6 +4,12 @@ document.addEventListener('page:loaded', () => {
   if (!CONFIG.enableMath) return;
 
   if (typeof MathJax === 'undefined') {
+    const output = {
+      displayOverflow: CONFIG.mathjax.display_overflow
+    };
+    if (CONFIG.mathjax.font_path) {
+      output.fontPath = CONFIG.mathjax.font_path;
+    }
     window.MathJax = {
       tex: {
         inlineMath: { '[+]': [['$', '$']] },
@@ -12,7 +18,7 @@ document.addEventListener('page:loaded', () => {
       options: {
         renderActions: {
           insertedScript: [200, () => {
-            document.querySelectorAll('mjx-container').forEach(node => {
+            document.querySelectorAll('mjx-container:not([display])').forEach(node => {
               const target = node.parentNode;
               if (target.nodeName.toLowerCase() === 'li') {
                 target.parentNode.classList.add('has-jax');
@@ -20,7 +26,8 @@ document.addEventListener('page:loaded', () => {
             });
           }, '', false]
         }
-      }
+      },
+      output
     };
     NexT.utils.getScript(CONFIG.mathjax.js, {
       attributes: {
